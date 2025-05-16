a2f0d47512617ad6513440c8e39f4da3582774c4
Mayra Elizabeth Moreno Velasquez <131404080+May-Moreno@users.noreply.github.com>
2025-05-16 13:02:54 -0600

    Add files via upload

---
+++ Reto1Sesion5/src/Patrulla.java
@@ -0,0 +1,24 @@
+public class Patrulla extends UnidadEmergencia{
+    private SistemaGPS gps;
+    private Sirena sirena;
+    private Operador operador;
+
+    public Patrulla(String nombre, Operador operador){
+        super(nombre);
+        this.gps = new SistemaGPS();
+        this.sirena = new Sirena();
+        this.operador = operador;
+    }
+    @Override
+    public void responder(){
+        System.out.println("Patrulla atendiendo situacion de seguridad ciudadana");
+    }
+    public void  iniciarOperacion(){
+        activarUnidad();
+        gps.localizar();
+        sirena.activarSirena();
+        operador.reportarse();
+        responder();
+    }
+
+}
